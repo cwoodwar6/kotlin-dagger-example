96bdfebfa35a8abb390be6d6d496f371af39a56f
Chris Woodward <cwoodward@squareup.com>
2017-09-07 13:20:31 -0700

    Strip the app down to just kapt for now and remove all other unnecessary things

--- app/src/main/java/org/loop/example/MainActivity.kt
+++ app/src/main/java/org/loop/example/MainActivity.kt
@@ -1,16 +1,15 @@
 package org.loop.example
 
+//import kotlinx.android.synthetic.main.activity_main.*
+import android.app.Activity
 import android.location.LocationManager
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.Menu
 import android.view.MenuItem
-import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 import javax.inject.Named
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     val TAG = MainActivity::class.java.name
 
@@ -27,14 +26,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         MyApplication.graph.inject(this)
-        assert(textView != null)
+//        assert(textView != null)
         Log.d(TAG, "$something and $somethingElse")
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -42,11 +35,6 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         val id = item.itemId
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true
-        }
 
         return super.onOptionsItemSelected(item)
     }
